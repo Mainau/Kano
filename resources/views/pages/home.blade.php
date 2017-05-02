1b81d6940cb05c304a23045a4c32f82997fe1019
Mainau <felix.thcilhcs@gmail.com>
2017-05-02 10:33:08 +0200

    s

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layout')
+
+@section('content')
+<ul class="collection with-header">
+    <li class="collection-header">
+        <h2 class="flow-text">Recent Surveys
+            <span style="float:right;">
+            </span>
+        </h2>
+    </li>
+
+</ul>
+@stop
