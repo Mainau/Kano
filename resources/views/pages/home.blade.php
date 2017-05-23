464c290281d074d922e39576ed5ff0191ea47424
Mainau <felix.thcilhcs@gmail.com>
2017-05-23 11:16:19 +0200

    nav link auskommentiert

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
   <nav class="htwg-darkblue" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Kano</a></li>
+      <!--  <li><a href="#">Kano</a></li> -->
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
