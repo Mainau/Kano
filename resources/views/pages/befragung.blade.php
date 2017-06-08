37f024db3221384fed5e38dc16d329eaa768707d
Nickerchen <jamesfriesen94@gmail.com>
2017-06-08 14:37:41 +0200

    fixed radio buttons

--- resources/views/pages/befragung.blade.php
+++ resources/views/pages/befragung.blade.php
@@ -44,19 +44,21 @@
         <tr>
           <td>
             <p> Bitte bewerten Sie die Anforderungen</p>
-                <input type="radio" id="Das würde mir sehr helfen"> Das würde mir sehr helfen </input><br/>
-                <input type="radio" id="Das ist eine grundlegende Anforderung für mich"> Das ist eine grundlegende Anforderung für mich </input><br/>
-                <input type="radio" id="Das würde mich nicht betreffen"> Das würde mich nicht betreffen </input><br/>
-                <input type="radio" id="Das wäre eine geringe Unanehmlichkeit für mich"> Das wäre eine geringe Unannehmlichkeit für mich </input><br/>
-                <input type="radio" id="Das wäre ein großes Problem für mich"> Das wäre ein großes Problem für mich </input><br/>
+                <form action="">
+                <input type="radio" name="rating" id="Das würde mir sehr helfen"> Das würde mir sehr helfen </input><br/>
+                <input type="radio" name="rating" id="Das ist eine grundlegende Anforderung für mich"> Das ist eine grundlegende Anforderung für mich </input><br/>
+                <input type="radio" name="rating" id="Das würde mich nicht betreffen"> Das würde mich nicht betreffen </input><br/>
+                <input type="radio" name="rating" id="Das wäre eine geringe Unanehmlichkeit für mich"> Das wäre eine geringe Unannehmlichkeit für mich </input><br/>
+                <input type="radio" name="rating" id="Das wäre ein großes Problem für mich"> Das wäre ein großes Problem für mich </input><br/>
+                </form>
           </td>
           <td>
             <p> Wie wichtig ist diese Anforderung für Sie?</p>
-            <input type="radio" id="Überaus wichtig"> Überaus wichtig </input><br/>
-                     <input type="radio" id="sehr wichtig"> Sehr wichtig </input><br/>
-                     <input type="radio" id="wichtig"> Wichtig </input><br/>
-                     <input type="radio" id="Ein wenig wichtig"> Ein wenig wichtig </input><br/>
-                     <input type="radio" id="Überhaupt nicht wichtig"> Überhaupt nicht wichtig </input>
+                 <input type="radio" name="importance" id="Überaus wichtig"> Überaus wichtig </input><br/>
+                 <input type="radio" name="importance" id="sehr wichtig"> Sehr wichtig </input><br/>
+                 <input type="radio" name="importance" id="wichtig"> Wichtig </input><br/>
+                 <input type="radio" name="importance" id="Ein wenig wichtig"> Ein wenig wichtig </input><br/>
+                 <input type="radio" name="importance" id="Überhaupt nicht wichtig"> Überhaupt nicht wichtig </input>
           </td>
         </tr>
       </table>
