475d429987fb4d4a18ad1fb9d9a919f3852f4e01
Nickerchen <jamesfriesen94@gmail.com>
2017-06-19 21:51:57 +0200

    fixed erstellung

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -9,16 +9,19 @@ class SurveyController extends Controller
 {
     public function store(Survey $survey)
     {
-
         $this->validate(request(), [
 
             'name' => 'required',
             'beschreibung' => 'required'
         ]);
 
-        auth()->user()->publish_survey(
+
+         auth()->user()->publish_survey(
             new Survey(request(['name', 'beschreibung']))
         );
+
+        $survey = Survey::orderBy('created_at', 'desc')->first();
+
 
 
         return redirect('/eingabe/' . $survey->id );
