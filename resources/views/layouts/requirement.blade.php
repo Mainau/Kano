6500a011255fb6bb9dd38441a97e039de437fc90
Nickerchen <jamesfriesen94@gmail.com>
2017-06-20 09:51:50 +0200

    requirements verlangen jetzt eingabe

--- resources/views/layouts/requirement.blade.php
+++ resources/views/layouts/requirement.blade.php
@@ -7,15 +7,15 @@
     <tr>
         <td>
             <p> Bitte bewerten Sie die Anforderungen</p>
-                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="entzueckt" id="helpful"> Das würde mir sehr helfen </input><br/>
-                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="wird erwartet" id="basic"> Das ist eine grundlegende Anforderung für mich </input><br/>
-                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="gleichgueltig" id="irrelevant"> Das würde mich nicht betreffen </input><br/>
-                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="stoert" id="annoying"> Das wäre eine geringe Unannehmlichkeit für mich </input><br/>
-                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="frustriert" id="problematic"> Das wäre ein großes Problem für mich </input><br/>
+                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="entzueckt" id="helpful" required> Das würde mir sehr helfen </input><br/>
+                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="wird erwartet" id="basic" required> Das ist eine grundlegende Anforderung für mich </input><br/>
+                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="gleichgueltig" id="irrelevant" required> Das würde mich nicht betreffen </input><br/>
+                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="stoert" id="annoying" required> Das wäre eine geringe Unannehmlichkeit für mich </input><br/>
+                <input type="radio" name="{{ $requirement->id }}[func_answer]" value="frustriert" id="problematic" required> Das wäre ein großes Problem für mich </input><br/>
         </td>
         <td>
             <p> Wie wichtig ist diese Anforderung für Sie?</p>
-            <input type="radio" name="{{ $requirement->id }}[imp_answer]" value="extrem wichtig" id="extremely_important"> Überaus wichtig </input><br/>
+            <input type="radio" name="{{ $requirement->id }}[imp_answer]" value="extrem wichtig" id="extremely_important" required> Überaus wichtig </input><br/>
             <input type="radio" name="{{ $requirement->id }}[imp_answer]" value="sehr wichtig" id="very_important"> Sehr wichtig </input><br/>
             <input type="radio" name="{{ $requirement->id }}[imp_answer]" value="wichtig" id="important"> Wichtig </input><br/>
             <input type="radio" name="{{ $requirement->id }}[imp_answer]" value="weniger wichtig" id="less_important"> Ein wenig wichtig </input><br/>
@@ -35,7 +35,7 @@
     <tr>
         <td>
             <p> Bitte bewerten Sie die Anforderungen</p>
-                <input type="radio" name="{{ $requirement->id }}[dys_answer]" value="entzueckt" id="helpful"> Das würde mir sehr helfen </input><br/>
+                <input type="radio" name="{{ $requirement->id }}[dys_answer]" value="entzueckt" id="helpful" required> Das würde mir sehr helfen </input><br/>
                 <input type="radio" name="{{ $requirement->id }}[dys_answer]" value="wird erwartet" id="basic"> Das ist eine grundlegende Anforderung für mich </input><br/>
                 <input type="radio" name="{{ $requirement->id }}[dys_answer]" value="gleichgueltig" id="irrelevant"> Das würde mich nicht betreffen </input><br/>
                 <input type="radio" name="{{ $requirement->id }}[dys_answer]" value="stoert" id="annoying"> Das wäre eine geringe Unannehmlichkeit für mich </input><br/>
