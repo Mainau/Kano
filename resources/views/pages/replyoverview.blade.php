8c29c9ab4f9f2533ca7d30c2cc2b16a969d7a04c
janina <jabeck@htwg-konstanz.de>
2017-06-20 10:10:15 +0200

    replyoverview angepasst

--- resources/views/pages/replyoverview.blade.php
+++ resources/views/pages/replyoverview.blade.php
@@ -1,6 +1,34 @@
 @extends('layout')
 
 @section('content')
+
+
+  <nav class="htwg-darkblue" role="navigation">
+    <!-- <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Kano</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div> -->
+    <div class="nav-wrapper container">
+
+      <!--  <img src="KANO-LOGO.jpg"  width="80px" height="80px"/> -->
+
+    </div>
+  </nav>
+  <style type="text/css">
+    #angemeldetAls{
+      visibility: hidden;
+    }
+    #logoutheader{
+      visibility: hidden;
+    }
+  </style>
+
 <style>
   table, th, td {
   border: 1px solid black;
